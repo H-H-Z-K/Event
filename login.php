1db8539775a40c62bbaeaac16eb152fd57f8b0e2
Hiba-abouhjeily <your.email@example.com>
2025-02-03 23:20:19 +0200

    git commit "a"

--- login.php
+++ login.php
@@ -2,20 +2,50 @@
 require_once 'includes/session_config.php';
 require_once 'includes/login_view.inc.php';
 ?>
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="stylelog.css?U=">
 </head>
 <body>
-<form action="includes/login.inc.php" method="post">
-        <input type="text" name="name" placeholder="Name">
-       
-        <input type="password" name="password" placeholder="Password">
-        <input type="submit" name="submit" value="login">
-    </form>
+    <div class="container">
+        <div class="form-box" id="form-container">
+            <img src="images/BLACK LOGO.JPG" alt="event manager ai" class="logo" />
+
+             <!-- LOG IN FORM -->
+            <div id="login-form">
+            <h2>LOG IN</h2>
+                <form action="includes/login.inc.php" method="post">
+                   <input type="text" name="name" placeholder="Name">
+                   <input type="password" name="password" placeholder="Password">
+                  <input type="submit" name="submit" value="login">
+               </form>
+         </div>
+     </div>
+      </div>
+      <script>
+               function showLogin() {
+        fetch("login.php")
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("HTTP error! Status: " + response.status);
+                }
+                return response.text();
+            })
+            .then(data => {
+                document.getElementById("form-container").innerHTML = data;
+            })
+            .catch(error => console.error('Error loading login page:', error));
+    }
+        </script>
     <?php
         check_signup_errors();
     ?>

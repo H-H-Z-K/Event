28dd671c9b2aebb4467a93b9a9c8296cd51c6dc5
Hussien320 <hussienzoughaib@gmail.com>
2025-02-04 12:27:34 +0200

    git commit "a";

--- dashbaord.php
+++ dashbaord.php
@@ -11,7 +11,7 @@ $events = list_event($pdo);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard</title>
-    <link rel="stylesheet" href="styles.css">
+
 </head>
 <body>
 
